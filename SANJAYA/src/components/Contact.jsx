37ab3c3b0eb3cbbd7f7aa4aa23cd3ee7fdd1141b
Sanjaya Niroula <niroulasanjaya42@fmail.com>
2024-05-08 18:13:15 +0545

    Near Copmletion

--- SANJAYA/src/components/Contact.jsx
+++ SANJAYA/src/components/Contact.jsx
@@ -6,6 +6,8 @@ function Contact() {
 
   const [firstName, setFirstName] = useState('');
 
+
+
   const {setUser}= useContext(UserContext);
 
   const {user}= useContext(UserContext);
@@ -15,22 +17,25 @@ function Contact() {
       e.preventDefault()
       setUser({firstName})
   }
+
+  
   return (
    
-    <div class="contact-container">
-        <form >
+    <div className="contact-container">
+        <form>
             <h1 className='font-bold text-3xl'>In Case You Need It.</h1>
             <br />
 
             {user ? <div className='w-full text-center text-2xl font-bold text-purple-400 '>Thank You {user.firstName} ! </div> : <h1 className='font-bold text-2xl'>Send Message Please</h1>}
       
-            <input type="text" id="firstname" placeholder="First Name" value={firstName} onChange={(e)=> setFirstName(e.target.value)} required></input>
-            <input type="text" id="lastname" placeholder="Last Name" required></input>
-            <input type="email"  id="email" placeholder="E-mail" required></input>
-            <input type="text"  id="mobile" placeholder="Mobile" required></input>
+            <input type="text" id="firstname" name='fname' placeholder="First Name" value={firstName} onChange={(e)=> setFirstName(e.target.value)} required></input>
+            <input type="text" id="lastname" name='lname' placeholder="Last Name" required></input>
+            <input type="email"  id="email" name='email' placeholder="E-mail" required></input>
+            <input type="text"  id="mobile" name='mobile' placeholder="Mobile" required></input>
             <h4 className='font-bold text-2xl'>Type Your Message Here...</h4>
-            <textarea required></textarea>
-            <input type="submit" value="Send" id="button" onClick={handleSubmit}></input>
+            <textarea name='textbox' required></textarea>
+            <input type="submit" value="Send" id="button" onClick={handleSubmit}
+            ></input>
 
         </form>
     </div>
--- SANJAYA/src/components/Hero.jsx
+++ SANJAYA/src/components/Hero.jsx
@@ -11,7 +11,15 @@ function Hero() {
           <h1 className="font-black text-5xl heading1">I'M GLAD</h1>
           <h1 className="font-black text-5xl heading1">YOU'VE COME THIS FAR</h1>
         </div>
+<div className="socials">
+  <ul>
+    <div className="one-social"><li>Instagram <span className="icon"><i class="ri-instagram-line"></i></span></li></div>
+    <div className="one-social"><li>Facebook <span className="icon"><i class="ri-facebook-line"></i></span></li></div>
+    <div className="one-social"><li>Twitter <span className="icon"><i class="ri-twitter-line"></i></span></li></div>
+    <div className="one-social"><li>LinkedIn <span className="icon"><i class="ri-linkedin-line"></i></span></li></div>
 
+  </ul>
+</div>
         <h3 className="font-bold my-name">I'M SANJAYA NIROULA </h3>
         <h3 className="font-bold frontend-text">FRONTEND DEVELOPER</h3>
       </div>
